b3fc37861d9217a21b8855caa3b8b745b409eb07
Joël Franusic <joel.franusic@okta.com>
2015-01-16 15:11:14 -0800

    Update statuses and add IntelliSense comments.
    Added "MFA_ENROLL" status.
    Removed "MFA_UNENROLLED", which seems to be unused or outdated?
    Added documentation descriptions from developer.okta.com as IntelliSense comments on each status.

--- Okta.Core/Models/Auth/AuthStatus.cs
+++ Okta.Core/Models/Auth/AuthStatus.cs
@@ -6,18 +6,31 @@ using System.Text;
 namespace Okta.Core.Models
 {
     /// <summary>
-    /// All possible statuses during authentication.
+    /// The status of an authentication or recovery transaction.
     /// </summary>
+    /// <remarks>
+    /// Full documentation available at: http://developer.okta.com/docs/api/rest/authn.html#authentication-status
+    /// </remarks>
     public static class AuthStatus
     {
-        public const string Success = "SUCCESS";
-        public const string MfaUnenrolled = "MFA_UNENROLLED";
-        public const string MfaEnrollActivate = "MFA_ENROLL_ACTIVATE";
-        public const string MfaRequired = "MFA_REQUIRED";
-        public const string MfaChallenge = "MFA_CHALLENGE";
+        // (Sorted according to the "Authentication Status" table in the URL above.)
+        /// <summary>The user’s password was successfully validated but is expired.</summary>
         public const string PasswordExpired = "PASSWORD_EXPIRED";
+        /// <summary>The user has requested a recovery token to reset their password or unlock their account.</summary>
         public const string Recovery = "RECOVERY";
-        public const string LockedOut = "LOCKED_OUT";
+        /// <summary>The user successfully answered their recovery question and must to set a new password.</summary>
         public const string PasswordReset = "PASSWORD_RESET";
+        /// <summary>The user account is locked; self-service unlock or admin unlock is required.</summary>
+        public const string LockedOut = "LOCKED_OUT";
+        /// <summary>The user must select and enroll an available factor for additional verification.</summary>
+        public const string MfaEnroll = "MFA_ENROLL";
+        /// <summary>The user must activate the factor to complete enrollment.</summary>
+        public const string MfaEnrollActivate = "MFA_ENROLL_ACTIVATE";
+        /// <summary>The user must provide additional verification with a previously enrolled factor.</summary>
+        public const string MfaRequired = "MFA_REQUIRED";
+        /// <summary>The user must verify the factor-specific challenge.</summary>
+        public const string MfaChallenge = "MFA_CHALLENGE";
+        /// <summary>The transaction has completed successfully.</summary>
+        public const string Success = "SUCCESS";
     }
 }
