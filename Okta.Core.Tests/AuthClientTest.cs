3b6a8facdc8c52f276747df019017370d0380dc4
Len Boyette <lboyette@okta.com>
2015-01-16 15:21:56 -0800

    Fixed casting to OktaAuthenticationException

---
+++ Okta.Core.Tests/AuthClientTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Okta.Core.Clients;
+
+namespace Okta.Core.Tests
+{
+    [TestClass]
+    public class AuthClientTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(OktaAuthenticationException))]
+        public void AuthExceptionTest()
+        {
+            var testConfig = new OktaSettings();
+            testConfig.ApiToken = "00UeGvRv5SKYzuWgqIg1ycd4uqk2dfgyZq0eAtSMHA";
+            testConfig.BaseUri = new Uri("http://rain.okta1.com:1802");
+            var client = new AuthClient(testConfig);
+
+            client.Authenticate("fakeusername", "fakepassword");
+        }
+    }
+}
