5c80cc9f49970a3df1f0bb0ed4651536e282a759
Joël Franusic <joel.franusic@okta.com>
2015-01-16 16:26:37 -0800

    Update Okta.Core to support TOTP Factors

--- Okta.Core/Clients/AuthClient.cs
+++ Okta.Core/Clients/AuthClient.cs
@@ -40,8 +40,16 @@ namespace Okta.Core.Clients
 
         public AuthResponse Verify(string stateToken, Factor factor, MfaAnswer answer = null)
         {
-            var verifyLink = factor.Links["verify"].First();
+            var verifyLink = factor.Links["verify"].Href;
             return Execute(stateToken, verifyLink, answer);
+        }
+
+        public AuthResponse ActivateTotpFactor(string stateToken, AuthResponse authResponse, string passCode)
+        {
+            var apiObject = new ApiObject();
+            apiObject.SetProperty("passCode", passCode);
+            var nextLink = authResponse.Links["next"];
+            return Execute(stateToken, nextLink, apiObject);
         }
 
         public AuthResponse ValidateToken(string recoveryToken)
