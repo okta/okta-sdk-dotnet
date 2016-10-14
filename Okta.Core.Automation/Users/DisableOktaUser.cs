923000f91390f185b2d6e4bc7df94d5089643280
Raphael Londner <raphael.londner@okta.com>
2016-10-14 13:42:05 -0700

    Ignored target framework version mismatch in Okta.Core.Automation for System.Management.Automation

--- Okta.Core.Automation/Users/DisableOktaUser.cs
+++ Okta.Core.Automation/Users/DisableOktaUser.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Management.Automation;
+﻿using System.Management.Automation;
 using Okta.Core.Models;
 
 namespace Okta.Core.Automation
