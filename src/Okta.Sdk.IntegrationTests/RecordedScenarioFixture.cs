4154e94130ce59f76d9b46ff428a1fd3e04d039c
Nate Barbettini <nathanaelb@gmail.com>
2017-06-22 15:10:24 -0700

    Add support for launching test-server

--- src/Okta.Sdk.IntegrationTests/RecordedScenarioFixture.cs
+++ src/Okta.Sdk.IntegrationTests/RecordedScenarioFixture.cs
@@ -4,14 +4,17 @@
 // </copyright>
 
 using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 
 namespace Okta.Sdk.IntegrationTests
 {
+#pragma warning disable SA1503 // Braces must not be omitted
     public sealed class RecordedScenarioFixture : IDisposable
     {
         private readonly bool _useLocalServer;
+        private readonly Process _localServerProcess;
 
         private bool _isDisposed = false; // To detect redundant calls
 
@@ -19,22 +22,44 @@ namespace Okta.Sdk.IntegrationTests
         {
             _useLocalServer = TestConfiguration.UseLocalServer;
 
-            if (!_useLocalServer)
-            {
-                return;
-            }
+            if (!_useLocalServer) return;
 
             Console.WriteLine("Starting local server...");
+            _localServerProcess = StartLocalServer();
+        }
+
+        private Process StartLocalServer()
+        {
+            var environmentPath = Environment.GetEnvironmentVariable("PATH");
+            var npmPath = environmentPath
+                .Split(';')
+                .Select(x => Path.Combine(x, "npm.cmd"))
+                .Where(File.Exists)
+                .FirstOrDefault();
+            Console.WriteLine("Found npm path: ", npmPath);
+
+            var startInfo = new ProcessStartInfo
+            {
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                FileName = npmPath,
+                Arguments = "run test-server",
+                WorkingDirectory = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "..", ".."),
+            };
+
+            return Process.Start(startInfo);
         }
 
         private void CleanUpLocalServer()
         {
-            if (!_useLocalServer)
-            {
-                return;
-            }
+            if (!_useLocalServer) return;
 
-            // stop!
+            if (_localServerProcess == null || _localServerProcess.HasExited) throw new Exception("The test server is not running.");
+
+            _localServerProcess.Dispose();
+
+            if (!_localServerProcess.HasExited) throw new Exception("The test server could not be stopped.");
+            if (_localServerProcess.ExitCode != 0) throw new Exception("Some test scenarios were not exercised.");
         }
 
         private void Dispose(bool disposing)
@@ -58,3 +83,4 @@ namespace Okta.Sdk.IntegrationTests
         }
     }
 }
+#pragma warning restore SA1503 // Braces must not be omitted
