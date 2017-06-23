2d158ea213b937d79d8858fdca3d53bafbfe52a3
Nate Barbettini <nathanaelb@gmail.com>
2017-06-23 10:45:50 -0700

    Add User convenience methods

--- src/Okta.Sdk/UserClient.cs
+++ src/Okta.Sdk/UserClient.cs
@@ -3,12 +3,35 @@
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 // </copyright>
 
+using System;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Okta.Sdk
 {
     public sealed partial class UserClient : OktaClient, IUserClient, IAsyncEnumerable<User>
     {
+        /// <inheritdoc/>
+        public Task<User> CreateUserAsync(UserWithPasswordCreationOptions createUserOptions, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (createUserOptions == null)
+            {
+                throw new ArgumentNullException(nameof(createUserOptions));
+            }
+
+            var user = new User
+            {
+                Profile = createUserOptions.Profile,
+                Credentials = new UserCredentials
+                {
+                    Password = new PasswordCredential { Value = createUserOptions.Password },
+                },
+            };
+
+            return CreateUserAsync(user, createUserOptions.Activate, cancellationToken: cancellationToken);
+        }
+
         /// <inheritdoc/>
         public IAsyncEnumerator<User> GetEnumerator() => ListUsers().GetEnumerator();
     }
---
+++ src/Okta.Sdk/UserWithPasswordCreationOptions.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="UserWithPasswordCreationOptions.cs" company="Okta, Inc">
+// Copyright (c) 2014-2017 Okta, Inc. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace Okta.Sdk
+{
+    public sealed class UserWithPasswordCreationOptions
+    {
+        public UserProfile Profile { get; set; }
+
+        public bool Activate { get; set; } = true;
+
+        public string Password { get; set; }
+    }
+}
