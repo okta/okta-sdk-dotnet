00ba866c5c9404d4722e81e85d116edb96156823
Nate Barbettini <nathanaelb@gmail.com>
2017-07-10 19:05:54 -0700

    Add additional CreateUser overloads

--- src/Okta.Sdk/IUsersClient.cs
+++ src/Okta.Sdk/IUsersClient.cs
@@ -13,12 +13,42 @@ namespace Okta.Sdk
     public partial interface IUsersClient : IAsyncEnumerable<IUser>
     {
         /// <summary>
+        /// Creates a new user in your Okta organization without a password or a recovery question/answer.
+        /// </summary>
+        /// <remarks>See <a href="https://developer.okta.com/docs/api/resources/users.html#create-user-without-credentials">Create User without Credentials</a> in the documentation.</remarks>
+        /// <param name="options">The options for this Create User (without credentials) request.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>The created user.</returns>
+        Task<IUser> CreateUserAsync(CreateUserWithoutCredentialsOptions options, CancellationToken cancellationToken = default(CancellationToken));
+
+        /// <summary>
+        /// Creates a new user in your Okta organization with a recovery question/answer (but no password).
+        /// </summary>
+        /// <remarks>See <a href="https://developer.okta.com/docs/api/resources/users.html#create-user-with-recovery-question">Create User with Recovery Question</a> in the documentation.</remarks>
+        /// <param name="options">The options for this Create User (with recovery question) request.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>The created user.</returns>
+        Task<IUser> CreateUserAsync(CreateUserWithRecoveryQuestionOptions options, CancellationToken cancellationToken = default(CancellationToken));
+
+        /// <summary>
         /// Creates a new user in your Okta organization with the specified password.
         /// </summary>
+        /// <remarks>See <a href="https://developer.okta.com/docs/api/resources/users.html#create-user-with-password">Create User with Password</a> in the documentation.</remarks>
         /// <param name="options">The options for this Create User (with password) request.</param>
         /// <param name="cancellationToken">The cancellation token.</param>
         /// <returns>The created user.</returns>
         Task<IUser> CreateUserAsync(CreateUserWithPasswordOptions options, CancellationToken cancellationToken = default(CancellationToken));
+
+        /// <summary>
+        /// Creates a new user in your Okta organization with the specified authentication provider.
+        /// </summary>
+        /// <remarks>See <a href="https://developer.okta.com/docs/api/resources/users.html#create-user-with-authentication-provider">Create User with Authentication Provider</a> in the documentation.</remarks>
+        /// <param name="options">The options for this Create User (with authentication provider) request.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>The created user.</returns>
+        Task<IUser> CreateUserAsync(CreateUserWithProviderOptions options, CancellationToken cancellationToken = default(CancellationToken));
+
+        // TODO add groupIds options to all of the above?
 
         /// <summary>
         /// Changes a user's password.
