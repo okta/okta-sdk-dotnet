9f2330a00555ecbf9fa325906814425a1046981a
Len Boyette <lboyette@okta.com>
2017-11-10 09:59:21 -0800

    Add additional convenience factors

---
+++ src/Okta.Sdk/AddHardwareFactorOptions.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="AddHardwareFactorOptions.cs" company="Okta, Inc">
+// Copyright (c) 2014-2017 Okta, Inc. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Okta.Sdk
+{
+    public sealed class AddHardwareFactorOptions
+    {
+        public string CredentialId { get; set; }
+        public string PassCode { get; set; }
+    }
+}
---
+++ src/Okta.Sdk/AddTokenFactorOptions.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="AddTokenFactorOptions.cs" company="Okta, Inc">
+// Copyright (c) 2014-2017 Okta, Inc. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Okta.Sdk
+{
+    public sealed class AddTokenFactorOptions
+    {
+        public string CredentialId { get; set; }
+        public string PassCode { get; set; }
+        public string NextPassCode { get; set; }
+    }
+}
