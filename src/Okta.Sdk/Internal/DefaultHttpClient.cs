6f8d0f7c74f286aea6b9d4d7d15122ad2e1c7209
Nate Barbettini <nathanaelb@gmail.com>
2018-06-12 15:00:02 -0700

    Fix StyleCop build errors

--- src/Okta.Sdk/Internal/DefaultHttpClient.cs
+++ src/Okta.Sdk/Internal/DefaultHttpClient.cs
@@ -1,5 +1,5 @@
 ﻿// <copyright file="DefaultHttpClient.cs" company="Okta, Inc">
-// Copyright (c) 2014-2017 Okta, Inc. All rights reserved.
+// Copyright (c) 2014 - present Okta, Inc. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 // </copyright>
 
