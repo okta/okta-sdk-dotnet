5dadf3a406920e620ca63e3ba34412b125f91668
Daniel Hull <2030639+dnlhll@users.noreply.github.com>
2018-06-21 17:35:24 -0400

    :seedling: Exposes the next link header as a continuation token
    Exposes the next link header (https://developer.okta.com/docs/api/getting_started/design_principles#pagination) as a continuation token when listing users or groups

---
+++ src/Okta.Sdk/PageOfResults.cs
@@ -0,0 +1,40 @@
+﻿// <copyright file="PageOfResults.cs" company="Okta, Inc">
+// Copyright (c) 2014 - present Okta, Inc. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+// </copyright>
+
+using System.Collections.Generic;
+
+namespace Okta.Sdk
+{
+    /// <summary>
+    /// Page of results
+    /// </summary>
+    /// <typeparam name="T">The type of the results</typeparam>
+    public class PageOfResults<T>
+    {
+        /// <summary>
+        /// Gets or sets the results.
+        /// </summary>
+        /// <value>
+        /// The results.
+        /// </value>
+        public IEnumerable<T> Results
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the continuation token.
+        /// </summary>
+        /// <value>
+        /// The continuation token.
+        /// </value>
+        public string ContinuationToken
+        {
+            get;
+            set;
+        }
+    }
+}
