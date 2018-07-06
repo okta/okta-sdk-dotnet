bfc199d9cc4ef68522fc87746e464a2a47123688
Nate Barbettini <nathanaelb@gmail.com>
2018-07-05 17:01:48 -0700

    Add missing file headers

--- src/Okta.Sdk/CreateBasicAuthApplicationOptions.cs
+++ src/Okta.Sdk/CreateBasicAuthApplicationOptions.cs
@@ -1,4 +1,9 @@
-﻿namespace Okta.Sdk
+﻿// <copyright file="CreateBasicAuthApplicationOptions.cs" company="Okta, Inc">
+// Copyright (c) 2014 - present Okta, Inc. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace Okta.Sdk
 {
     public sealed class CreateBasicAuthApplicationOptions
     {
