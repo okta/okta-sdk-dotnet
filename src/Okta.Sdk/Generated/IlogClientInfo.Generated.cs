612a2f9237d922ee640acf4988a3a24e2f63ef21
Laura Rodriguez <rdz.maria.laura@gmail.com>
2018-08-15 16:38:36 -0400

    Missing file.

--- src/Okta.Sdk/Generated/IlogClientInfo.Generated.cs
+++ src/Okta.Sdk/Generated/IlogClientInfo.Generated.cs
@@ -5,10 +5,6 @@
 
 // This file was automatically generated. Don't modify it directly.
 
-using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace Okta.Sdk
 {
