4fac744af250a42e519548a2d2dff52b85b244d0
Laura Rodriguez <rdz.maria.laura@gmail.com>
2018-08-17 12:28:12 -0400

    Update file after running generate

--- src/Okta.Sdk/Generated/ILogClientInfo.Generated.cs
+++ src/Okta.Sdk/Generated/ILogClientInfo.Generated.cs
@@ -5,6 +5,10 @@
 
 // This file was automatically generated. Don't modify it directly.
 
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Okta.Sdk
 {
