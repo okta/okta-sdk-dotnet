faa975125be9cd99e26f0263a24afe10f694f7c5
Laura Rodriguez <rdz.maria.laura@gmail.com>
2018-09-07 15:19:57 -0400

    Add unit test to validate that DisableHttpsCheck is defaulted to false.

---
+++ src/Okta.Sdk.UnitTests/OktaClientConfigurationShould.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="OktaClientConfigurationShould.cs" company="Okta, Inc">
+// Copyright (c) 2014 - present Okta, Inc. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+// </copyright>
+
+using FluentAssertions;
+using Okta.Sdk.Configuration;
+using Xunit;
+
+namespace Okta.Sdk.UnitTests
+{
+    public class OktaClientConfigurationShould
+    {
+        [Fact]
+        public void DefaultDisableHttpsCheckToFalse()
+        {
+            var clientConfiguration = new OktaClientConfiguration();
+
+            clientConfiguration.DisableHttpsCheck.Should().BeFalse();
+        }
+    }
+}
