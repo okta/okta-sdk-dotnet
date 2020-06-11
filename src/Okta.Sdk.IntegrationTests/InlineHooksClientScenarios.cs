062e08297d4df19aa7cc308de26f6f7ef80e6e55
Bryan Apellanes <bryan.apellanes@gmail.com>
2020-06-11 11:54:37 -0700

    address feedback

--- src/Okta.Sdk.IntegrationTests/InlineHooksClientScenarios.cs
+++ src/Okta.Sdk.IntegrationTests/InlineHooksClientScenarios.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="InlineHooksClientShould.cs" company="Okta, Inc">
+﻿// <copyright file="InlineHooksClientScenarios.cs" company="Okta, Inc">
 // Copyright (c) 2020 - present Okta, Inc. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 // </copyright>
