31ee8a92c0baf03ba0d77b947eda683baacf05bf
Laura Rodriguez <rdz.maria.laura@gmail.com>
2020-07-17 15:31:22 -0400

    Move parallelization configuration for ITs project to AssemblyInfo.cs.

--- src/Okta.Sdk.IntegrationTests/AssemblyInfo.cs
+++ src/Okta.Sdk.IntegrationTests/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="DisableTestParallelization.cs" company="Okta, Inc">
+﻿// <copyright file="AssemblyInfo.cs" company="Okta, Inc">
 // Copyright (c) 2020 - present Okta, Inc. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 // </copyright>
