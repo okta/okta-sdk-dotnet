fe07bd5d58628e15cfbdaffb12aa9f4ab8a23bc2
Laura Rodriguez <rdz.maria.laura@gmail.com>
2021-09-24 11:48:04 -0400

    Add UTs to cover authenticators API.

--- src/Okta.Sdk.UnitTests/AuthenticatorClientShould.cs
+++ src/Okta.Sdk.UnitTests/AuthenticatorClientShould.cs
@@ -61,8 +61,65 @@ namespace Okta.Sdk.UnitTests
             var mockRequestExecutor = new MockedStringRequestExecutor(rawResponse);
             var client = new TestableOktaClient(mockRequestExecutor);
 
-            //var response = await client.Authenticators.ActivateAuthenticatorAsync("aut1nd8PQhGcQtSxB0g4");
+            var authenticator = await client.Authenticators.ActivateAuthenticatorAsync("aut1nd8PQhGcQtSxB0g4");
+            authenticator.Id.Should().Be("aut1nd8PQhGcQtSxB0g4");
+            authenticator.Status.Should().Be(AuthenticatorStatus.Active);
+            authenticator.Key.Should().Be("webauthn");
+            authenticator.Name.Should().Be("Security Key or Biometric");
 
+            mockRequestExecutor.ReceivedHref.Should().Be("/api/v1/authenticators/aut1nd8PQhGcQtSxB0g4/lifecycle/activate");
+        }
+
+        [Fact]
+        public async Task DeactivateAuthenticatorAsync()
+        {
+            var rawResponse = @"{
+                                ""type"": ""security_key"",
+                                ""id"": ""aut1nd8PQhGcQtSxB0g4"",
+                                ""key"": ""webauthn"",
+                                ""status"": ""INACTIVE"",
+                                ""name"": ""Security Key or Biometric"",
+                                ""created"": ""2020-07-26T21:16:37.000Z"",
+                                ""lastUpdated"": ""2020-07-26T21:59:33.000Z"",
+                                ""_links"": {
+                                    ""self"": {
+                                        ""href"": ""https://${yourOktaDomain}/api/v1/authenticators/aut1nd8PQhGcQtSxB0g4"",
+                                        ""hints"": {
+                                            ""allow"": [
+                                                ""GET"",
+                                                ""PUT""
+                                            ]
+                                        }
+                                    },
+                                    ""methods"": {
+                                        ""href"": ""https://${yourOktaDomain}/api/v1/authenticators/aut1nd8PQhGcQtSxB0g4/methods"",
+                                        ""hints"": {
+                                            ""allow"": [
+                                                ""GET""
+                                            ]
+                                        }
+                                    },
+                                    ""deactivate"": {
+                                        ""href"": ""https://${yourOktaDomain}/api/v1/authenticators/aut1nd8PQhGcQtSxB0g4/lifecycle/deactivate"",
+                                        ""hints"": {
+                                            ""allow"": [
+                                                ""POST""
+                                            ]
+                                        }
+                                    }
+                                }
+                            }";
+
+            var mockRequestExecutor = new MockedStringRequestExecutor(rawResponse);
+            var client = new TestableOktaClient(mockRequestExecutor);
+
+            var authenticator = await client.Authenticators.DeactivateAuthenticatorAsync("aut1nd8PQhGcQtSxB0g4");
+            authenticator.Id.Should().Be("aut1nd8PQhGcQtSxB0g4");
+            authenticator.Status.Should().Be(AuthenticatorStatus.Inactive);
+            authenticator.Key.Should().Be("webauthn");
+            authenticator.Name.Should().Be("Security Key or Biometric");
+
+            mockRequestExecutor.ReceivedHref.Should().Be("/api/v1/authenticators/aut1nd8PQhGcQtSxB0g4/lifecycle/deactivate");
         }
     }
 }
