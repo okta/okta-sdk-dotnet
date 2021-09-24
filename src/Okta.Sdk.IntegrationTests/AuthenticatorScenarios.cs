fb57bb9f5b5451317a851582805545e9656fe3f5
Laura Rodriguez <rdz.maria.laura@gmail.com>
2021-09-24 12:06:24 -0400

    Add missing header.

--- src/Okta.Sdk.IntegrationTests/AuthenticatorScenarios.cs
+++ src/Okta.Sdk.IntegrationTests/AuthenticatorScenarios.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿// <copyright file="AuthenticatorScenarios.cs" company="Okta, Inc">
+// Copyright (c) 2020 - present Okta, Inc. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+// </copyright>
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
