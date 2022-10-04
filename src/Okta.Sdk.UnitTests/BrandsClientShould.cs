ce7f2f1105d8c6486e89501dad5a8e4e801f863d
Laura Rodriguez <rdz.maria.laura@gmail.com>
2022-10-04 14:03:48 -0400

    Convert flaky ITs to UTs.

--- src/Okta.Sdk.UnitTests/BrandsClientShould.cs
+++ src/Okta.Sdk.UnitTests/BrandsClientShould.cs
@@ -6,6 +6,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FluentAssertions;
@@ -99,7 +100,6 @@ namespace Okta.Sdk.UnitTests
             mockRequestExecutor.ReceivedHttpVerbs[0].Should().Be(HttpVerb.Delete);
         }
 
-
         [Fact]
         public async Task GetEmailCustomizationPreview()
         {
@@ -111,7 +111,104 @@ namespace Okta.Sdk.UnitTests
             mockRequestExecutor.ReceivedHref.Should().Be($"/api/v1/brands/foo/templates/email/bar/customizations/baz/preview");
             mockRequestExecutor.ReceivedHttpVerbs.Count.Should().Be(1);
             mockRequestExecutor.ReceivedHttpVerbs[0].Should().Be(HttpVerb.Get);
+        }
 
+        [Fact]
+        public async Task CreateEmailTemplateCustomization()
+        {
+            var rawResponse = @"{
+                ""body"": ""<!DOCTYPE html><html>...${activationLink}...</html>"",
+                ""subject"": ""Test Subject"",
+                ""language"": ""fr""}";
+            var mockRequestExecutor = new MockedStringRequestExecutor(rawResponse);
+
+            var client = new TestableOktaClient(mockRequestExecutor);
+
+            var emailTemplateCustomizationRequest = new EmailTemplateCustomizationRequest()
+            {
+                Body = "<!DOCTYPE html><html>...${activationLink}...</html>",
+                Subject = "Test Subject",
+                Language = "fr",
+                IsDefault = false,
+            };
+
+            var emailTemplateCustomization = await client.Brands.CreateEmailTemplateCustomizationAsync(emailTemplateCustomizationRequest, "foo", "bar");
+
+            mockRequestExecutor.ReceivedHref.Should().Be($"/api/v1/brands/foo/templates/email/bar/customizations");
+            mockRequestExecutor.ReceivedHttpVerbs.Count.Should().Be(1);
+            mockRequestExecutor.ReceivedHttpVerbs[0].Should().Be(HttpVerb.Post);
+
+            emailTemplateCustomization.Should().NotBeNull();
+            emailTemplateCustomization.Body.Should().Be(emailTemplateCustomizationRequest.Body);
+            emailTemplateCustomization.Subject.Should().Be(emailTemplateCustomizationRequest.Subject);
+            emailTemplateCustomization.Language.Should().Be(emailTemplateCustomizationRequest.Language);
+        }
+
+        [Fact]
+        public async Task DeleteEmailTemplateCustomization()
+        {
+            var mockRequestExecutor = new MockedStringRequestExecutor(string.Empty);
+            var client = new TestableOktaClient(mockRequestExecutor);
+
+            await client.Brands.DeleteEmailTemplateCustomizationAsync("foo", "bar", "baz");
+
+            mockRequestExecutor.ReceivedHref.Should().Be($"/api/v1/brands/foo/templates/email/bar/customizations/baz");
+            mockRequestExecutor.ReceivedHttpVerbs.Count.Should().Be(1);
+            mockRequestExecutor.ReceivedHttpVerbs[0].Should().Be(HttpVerb.Delete);
+        }
+
+        [Fact]
+        public async Task GetEmailTemplateCustomization()
+        {
+            var rawResponse = @"{
+                ""body"": ""<!DOCTYPE html><html>...${activationLink}...</html>"",
+                ""subject"": ""Test Subject"",
+                ""language"": ""fr""}";
+            var mockRequestExecutor = new MockedStringRequestExecutor(rawResponse);
+
+            var client = new TestableOktaClient(mockRequestExecutor);
+
+            var retrievedEmailTemplateCustomization = await client.Brands.GetEmailTemplateCustomizationAsync("foo", "bar", "baz");
+
+            mockRequestExecutor.ReceivedHref.Should().Be($"/api/v1/brands/foo/templates/email/bar/customizations/baz");
+            mockRequestExecutor.ReceivedHttpVerbs.Count.Should().Be(1);
+            mockRequestExecutor.ReceivedHttpVerbs[0].Should().Be(HttpVerb.Get);
+
+            retrievedEmailTemplateCustomization.Should().NotBeNull();
+            retrievedEmailTemplateCustomization.Body.Should().Be("<!DOCTYPE html><html>...${activationLink}...</html>");
+            retrievedEmailTemplateCustomization.Subject.Should().Be("Test Subject");
+            retrievedEmailTemplateCustomization.Language.Should().Be("fr");
+        }
+
+        [Fact]
+        public async Task UpdateEmailTemplateCustomization()
+        {
+            var rawResponse = @"{
+                ""body"": ""Updated Body"",
+                ""subject"": ""Updated Subject"",
+                ""language"": ""fr""}";
+
+            var mockRequestExecutor = new MockedStringRequestExecutor(rawResponse);
+
+            var client = new TestableOktaClient(mockRequestExecutor);
+            var emailTemplateCustomizationUpdateRequest = new EmailTemplateCustomizationRequest()
+            {
+                Body = "Updated Body",
+                Subject = "Updated Subject",
+                Language = "fr",
+                IsDefault = false,
+            };
+
+            var updatedEmailTemplateCustomization = await client.Brands.UpdateEmailTemplateCustomizationAsync(emailTemplateCustomizationUpdateRequest, "foo", "bar", "baz");
+
+            mockRequestExecutor.ReceivedHref.Should().Be($"/api/v1/brands/foo/templates/email/bar/customizations/baz");
+            mockRequestExecutor.ReceivedHttpVerbs.Count.Should().Be(1);
+            mockRequestExecutor.ReceivedHttpVerbs[0].Should().Be(HttpVerb.Put);
+
+            updatedEmailTemplateCustomization.Should().NotBeNull();
+            updatedEmailTemplateCustomization.Body.Should().Be(emailTemplateCustomizationUpdateRequest.Body);
+            updatedEmailTemplateCustomization.Subject.Should().Be(emailTemplateCustomizationUpdateRequest.Subject);
+            updatedEmailTemplateCustomization.Language.Should().Be(emailTemplateCustomizationUpdateRequest.Language);
         }
     }
 }
