6665a7d64f00736c544bc8bbc4f86273905caa36
Laura Rodríguez <rdz.maria.laura@gmail.com>
2023-10-04 15:18:53 -0400

    Update the Links property of the VerifyUserFactorResponse model in the Open API spec to expose poll and cancel objects. (#671)

---
+++ docs/VerifyUserFactorResponseLinks.md
@@ -0,0 +1,12 @@
+# Okta.Sdk.Model.VerifyUserFactorResponseLinks
+
+## Properties
+
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+**Self** | [**HrefObjectSelfLink**](HrefObjectSelfLink.md) |  | [optional] 
+**Poll** | [**HrefObject**](HrefObject.md) |  | [optional] 
+**Cancel** | [**HrefObject**](HrefObject.md) |  | [optional] 
+
+[[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
+
---
+++ src/Okta.Sdk.UnitTest/Api/UserFactorApiTests.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq;
+using System.Net;
+using Xunit;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Okta.Sdk.UnitTest.Internal;
+using Okta.Sdk.Api;
+using Okta.Sdk.Client;
+using WireMock.Server;
+
+namespace Okta.Sdk.UnitTest.Api
+{
+    public class UserFactorApiTests
+    {
+        private readonly WireMockServer _server;
+
+        public UserFactorApiTests()
+        {
+            _server = WireMockServer.Start();
+        }
+
+        public void Dispose()
+        {
+            _server.Stop();
+        }
+
+        [Fact]
+        public async Task GetVerifyUserFactorResponseTransactionIdViaLinks()
+        {
+
+            var response = @"{
+                  ""expiresAt"": ""2015-04-01T15:57:32.000Z"",
+                  ""factorResult"": ""WAITING"",
+                  ""profile"":{
+                     ""credentialId"":""jane.doe@example.com""
+                  },
+                  ""_links"": {
+                    ""poll"": {
+                      ""href"": ""https://${yourOktaDomain}/api/v1/users/00u15s1KDETTQMQYABRL/factors/opfh52xcuft3J4uZc0g3/transactions/verificationTransactionId"",
+                      ""hints"": {
+                        ""allow"": [
+                          ""GET""
+                        ]
+                      }
+                    },
+                    ""cancel"": {
+                      ""href"": ""https://${yourOktaDomain}/api/v1/users/00u15s1KDETTQMQYABRL/factors/opfh52xcuft3J4uZc0g3/transactions/v2mst.GldKV5VxTrifyeZmWSQguA"",
+                      ""hints"": {
+                        ""allow"": [
+                          ""DELETE""
+                        ]
+                      }
+                    }
+                  }
+                }";
+
+            var mockClient = new MockAsyncClient(response, HttpStatusCode.OK);
+            var userFactorApi = new UserFactorApi(mockClient, new Configuration { BasePath = "https://foo.com" });
+
+            var verifyUserFactorResponse = await userFactorApi.VerifyFactorAsync("foo", "bar");
+
+            var transactionId = verifyUserFactorResponse.Links.Poll.Href.Split('/').Last();
+
+            transactionId.Should().Be("verificationTransactionId");
+        }
+    }
+}
