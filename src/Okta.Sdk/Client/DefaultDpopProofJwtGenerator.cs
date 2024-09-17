70069e71b8b4efbbadfb223a35d0f08e45c71c67
Bryan Apellanes <bryan.apellanes@okta.com>
2024-09-17 09:38:05 -0500

    use IdentityMode 8.0.2

--- src/Okta.Sdk/Client/DefaultDpopProofJwtGenerator.cs
+++ src/Okta.Sdk/Client/DefaultDpopProofJwtGenerator.cs
@@ -16,6 +16,7 @@ using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
 using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
 
 namespace Okta.Sdk.Client
 {
@@ -109,7 +110,7 @@ namespace Okta.Sdk.Client
                 
                 var outboundAlgorithmMap = new Dictionary<string, string> { { "alg", SecurityAlgorithms.RsaSha256 } };
 
-                var additionalHeaders = new Dictionary<string, object> { { "jwk", publicJsonWebKey } };
+                var additionalHeaders = new Dictionary<string, object> { { "jwk", JsonConvert.SerializeObject(publicJsonWebKey) } };
 
                 var securityToken =
                     new JwtSecurityToken(
