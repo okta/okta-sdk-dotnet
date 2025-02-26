e60c2643468c82ebae120de38d311b50737f73e3
Bryan Apellanes <bryan.apellanes@okta.com>
2025-02-26 13:35:57 -0600

    update script

--- .okta/regenerate.sh
+++ .okta/regenerate.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+if [[ -z "${CIRCLE_WORKING_DIRECTORY}" ]]; then
+  OKTA_SCRIPT_ROOT="."
+else
+  OKTA_SCRIPT_ROOT="${CIRCLE_WORKING_DIRECTORY}.okta"
+fi
+
+echo OKTA_SCRIPT_ROOT = ${OKTA_SCRIPT_ROOT}
+pushd ${OKTA_SCRIPT_ROOT}
+
 ./clean.sh
 
 ./generate.sh
+
+popd
