2b8c89fc01a643469aff3bb07104e08cd9ade25f
Aniket <aniket@okta.com>
2025-10-29 11:13:20 +0530

    Add: Copyright headers to all SDK files via OpenAPI generator templates

--- src/Okta.Sdk/Client/OAuthTokenResponse.cs
+++ src/Okta.Sdk/Client/OAuthTokenResponse.cs
@@ -1,4 +1,9 @@
-﻿/*
+﻿// <copyright company="Okta, Inc">
+// Copyright (c) 2014-present Okta, Inc. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+// </copyright>
+
+/*
  * Okta Admin Management
  *
  * Allows customers to easily access the Okta Management APIs
