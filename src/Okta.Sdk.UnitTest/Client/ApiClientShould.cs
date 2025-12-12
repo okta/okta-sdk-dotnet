508729de7e29551e1994225e945a2c31f80ef49c
Ben Peddell <bpeddell@tsv.catholic.edu.au>
2025-12-12 17:29:01 +1000

    Add tests for DPoP htu / htm claims

--- src/Okta.Sdk.UnitTest/Client/ApiClientShould.cs
+++ src/Okta.Sdk.UnitTest/Client/ApiClientShould.cs
@@ -203,6 +203,67 @@ namespace Okta.Sdk.UnitTest.Client
         }
 
         [Fact]
+        public async Task ExpandUrlInDpopHeadersWhenRetrying429Request()
+        {
+
+            Context pollyContext = new Context();
+            RestRequest request = new RestRequest("resource/{id}", Method.Get);
+
+
+            var jsonPrivateKey = @"{
+                                    ""p"":""2-8pgwYv9jrkM2KsbnQmnJZnr69Rsj95M20I1zx5HhM3tgjGSa7d_dELPRkp9Usy8UGISt7eUHpYOVl529irHwbXevuId1Q804aQ_AtNJwpbRY48rw2T8LdtyVSaEyoFMCa8PJwtzZYzKJCKAe5eoXvW5zxB65RaIct0igYcoIs"",
+                                    ""kty"":""RSA"",
+                                    ""q"":""slkNUY_SCIn95ip7HoPs_IIiNoKOGeesIV1gacyoAycly1vBPMhtar9gwx51nN1tCMVGlSOk583eRJe2omAbqkIEYm1jSWtMdJKQSOJvx812xbF1afMgJDlJ6iRIlcnWEYhNNMCK5s_UR5zE0Mc5jktxDFeiEatriyu6o9hQix8"",
+                                    ""d"":""LIpJTKCi9hPTiuUU954hayd3lXNwTVS6Fdny2iUj6iZ22eRp1V_UswECuMy5B-8lWbp1Gu_eASvhElSCB26m3UgHRVy8LP6Lmvm9VlJuZ5NtOK5D0R-gzFLINGdQH1PehzEc44jsTWyu297lgCLrVy-VScHQJodni3txTzxY4jwjILMfLB7OWdKVkvDQ4g70BYTVN5kZKjA9B0lLsofi1gUY_EVlojuvSKbm3HY0JR_JThtEd_nZw_tXTYmlP58plVYX-9JnA8NcFRB6dUNO7XqcXU1SafWqoM9yam1nGSMYRknwjSSTKRdBXHSy7PVxVHhpC72wb3aWNsOqWNo0ZQ"",
+                                    ""e"":""AQAB"",
+                                    ""qi"":""u1mS53N4BUSoMeOHFd0o4_n3QGH4izNUsiJVBIkv_UZUAk4LYudPEikTWRLqWsrcXmOyZYao5sSaZyt-B2XCkfdnkIhl-Q7b0_W0yt3Eh5XjAzH9oy5Dklog255lh-Y0yoWXvLjq-KEDs7Nd2uIT4gvKU4ymTqybvzazr2jY9qQ"",""dp"":""nCtPBsK1-9oFgJdoaWYApOAH8DBFipSXs3SQ-oTuW_S5coD4jAmniDuQB2p-6LblDXrDFKb8pZi6XL60UO-hUv7As4s4c8NVDb5X5SEBP9-Sv-koHgU-L4eQZY21ejY0SOS4dTFRNNKasQsxc_2XJIOTLc8T3_wPpD-cGQYN_dE"",""dq"":""ZWb4iZ0qICzFLW6N3gXIYrFi3ndQcC4m0jmTLdRs2o4RkRQ0RGj4vS7ex1G0MWI8MjZoMTe49Qs6Cunvr1bRo_YxI_1p7D6Tk9wZKTeFsqaBl1mUlo7jgXUJL5U9p9zAV-uVah7nWuBjo-vgg4wij2MZfZj9zuoWFWThk3LUKKU"",""n"":""mTjMc8AxU102LT1Jf-1qkGmaSiK4L7DDlC1SMvtyCRbDaiJDIagedfp1w8Pgud8YWOaS5FFx0S6JqGGP2U8OtpowzBcv5sYa-e5LHfnoueTJPj_jnI3fj5omZM1w-ofhFLPZoYEQ7DFYw0yLrzf8zaKB5-9BZ8yyOLhSKqxaOl2s7lw2TrwBRuQpPXmEir70oDPvazd8-An5ow6F5q7mzMtHAt61DJqrosRHiRwh4N37zIX_RNu-Tn1aMktCBl01rdoDyVq7Y4iwNH8ZAtT5thKK2eo8d-jb9TF9PH6LGffYCth157w-K4AZwXw74Ybo5NOux3XpIpKRbFTwvBLp1Q""
+                                 }";
+
+            var configuration = new Configuration();
+            configuration.Scopes = new HashSet<string> { "okta.users.read" };
+            configuration.ClientId = "foo";
+            configuration.PrivateKey = new JsonWebKeyConfiguration(jsonPrivateKey);
+            configuration.MaxRetries = 2;
+            configuration.AuthorizationMode = AuthorizationMode.PrivateKey;
+            configuration.OktaDomain = "https://mytestorg.com";
+
+            Queue<string> tokenReturnQueue = new Queue<string>();
+            tokenReturnQueue.Enqueue("myAccessToken2");
+
+            var oauthTokenProvider = new DefaultOAuthTokenProvider(configuration, new MockOAuthApi(configuration, returnQueue: tokenReturnQueue, isDpop: true));
+            var apiClient = new ApiClient(new OktaApiClientOptions(configuration, oauthTokenProvider));
+
+            var mockHttp = new MockHttpMessageHandler();
+
+            mockHttp
+                .When("https://mytestorg.com/resource/someResourceId")
+                .Respond(HttpStatusCode.OK, "application/json", "{}");
+
+
+            var client = new RestClient(options: new RestClientOptions { BaseUrl = new Uri(configuration.OktaDomain), ConfigureMessageHandler = _ => mockHttp });
+
+            pollyContext.Add(DefaultRetryStrategy.XOktaRequestId, "bar");
+            pollyContext.Add(DefaultRetryStrategy.XOktaRetryCountHeader, 2);
+
+            request.Parameters.AddParameter(new HeaderParameter("Authorization", "DPoP myAccessToken"));
+            request.Parameters.AddParameter(new HeaderParameter("DPoP", "myDpopJwt"));
+            request.Parameters.AddParameter(new UrlSegmentParameter("id", "someResourceId"));
+
+            var dpopHeader = request.Parameters.FirstOrDefault(x => x.Name == "DPoP");
+            dpopHeader.Value.Should().Be("myDpopJwt");
+
+            _ = await apiClient.ExecuteAsyncWithRetryHeadersAsync(pollyContext, request, client, configuration);
+
+            dpopHeader = request.Parameters.FirstOrDefault(x => x.Name == "DPoP");
+            dpopHeader.Value.Should().NotBe("myDpopJwt");
+            dpopHeader.Value.Should().BeAssignableTo(typeof(string));
+
+            var decodedJwt = new System.IdentityModel.Tokens.Jwt.JwtSecurityTokenHandler().ReadJwtToken((string)dpopHeader.Value);
+            decodedJwt.Payload["htu"].Should().Be("https://mytestorg.com/resource/someResourceId");
+            decodedJwt.Payload["htm"].Should().Be("GET");
+        }
+
+        [Fact]
         public async Task NotUpdateHeadersIfItIsNotRetry()
         {
 
@@ -388,7 +449,7 @@ namespace Okta.Sdk.UnitTest.Client
             dpopHeader = request.Parameters.FirstOrDefault(x => x.Name == "DPoP");
             dpopHeader.Value.Should().Be("myDpopJwt2");
         }
-        
+
         // Test for the conditions described in the github issue comment here: https://github.com/okta/okta-sdk-dotnet/issues/691#issuecomment-2130299272
         [Fact]
         public void NotFailOnEmptyProxyConfig()
